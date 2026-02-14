818e5f1db14fb6739189380e354b57c77cd80d78
xXx <181945610+ribel13@users.noreply.github.com>
2026-02-14 19:31:54 +0900

    Update fw25.sh

--- config-openwrt/imagebuilder/fw25.sh
+++ config-openwrt/imagebuilder/fw25.sh
@@ -38,7 +38,7 @@
 #
 # Set default parameters
 make_path="${PWD}"
-openwrt_dir="imagebuilder"
+openwrt_dir="openwrt"
 imagebuilder_path="${make_path}/${openwrt_dir}"
 custom_files_path="${make_path}/config-openwrt/imagebuilder/files"
 custom_config_file="${make_path}/config-openwrt/imagebuilder/config"
